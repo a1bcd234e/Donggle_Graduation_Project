cb2bf9d5b163036c0b01c5909dd8d2f1762e1663
a1bcd234e <51282271+a1bcd234e@users.noreply.github.com>
2024-07-13 20:28:04 +0900

    Revert "회원가입 xml, java 만듦"
    This reverts commit f789980ea87aa21597c5609a23c263d289de4bc1.

--- app/src/main/java/com/example/graduate/LoginActivity.java
+++ app/src/main/java/com/example/graduate/LoginActivity.java
@@ -2,21 +2,16 @@ package com.example.graduate;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Display;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
-    LinearLayout idLayout;
     EditText id, pw;
-    Button loginB, fpwB, signUpB;
+    Button loginB, fpwB;
     Button tabB, menu1, menu2, menu3;
 
     @Override
@@ -24,12 +19,10 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        idLayout = (LinearLayout) findViewById(R.id.idLayout);
         id = (EditText) findViewById(R.id.id);
         pw = (EditText) findViewById(R.id.pw);
         loginB = (Button) findViewById(R.id.loginB);
         fpwB = (Button) findViewById(R.id.fpwB);
-        signUpB = (Button) findViewById(R.id.signUpB);
 
         loginB.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,17 +35,7 @@ public class LoginActivity extends AppCompatActivity {
         fpwB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), PWActivity.class);
-
-                startActivity(intent);
-            }
-        });
-
-        signUpB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
-                startActivity(intent);
+                setContentView(R.layout.activity_fpw);
             }
         });
     }
